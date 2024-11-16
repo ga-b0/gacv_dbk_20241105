0f93138e2545ff9786685a306c19aba7664af0bf
Gabriel Calderón <gabrielvilleda8@gmail.com>
2024-11-15 19:37:55 -0600

    feat: :sparkles: add new controller, model and routing

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $validatedData = $request->validate([
+            'name'=>['required','string','max:255'],
+            'email'=>['required','string','email','max:255','unique:users'],
+            'password'=>['required','string','min:8','max:20'],
+        ]);
+
+        $user = User::create([
+            'name'=> $validatedData['name'],
+            'email'=> $validatedData['email'],
+            'password'=> Hash::make($validatedData['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            "success"=> true,
+            "errors"=>[
+                "code"=>0,
+                "msg"=>""
+            ],
+            "data"=>[
+                "access_token"=>$token,
+                "token_type" => "Bearer"
+            ],
+            "msg"=>"Usuario creado satisfactoriamente",
+            "count"=>1
+        ]);
+    }
+
+
+
+    public function login(Request $request) {
+        if(!Auth::attempt($request->only( "email","password"))){
+            return response()->json( [
+                "success"=> false,
+                "errors"=>[
+                    "code"=>401,
+                    "msg"=> "No se reconocen las credenciales"
+                ],
+                "data"=>"",
+                "count"=> 0
+            ], 401);
+
+        }
+        $user = User::where("email", $request->email)->firstOrFail();
+        $token = $user->createToken("auth_token")->plainTextToken;
+
+        return response()->json( [
+            "success"=> true,
+            "errors"=>[
+                "code"=>200,
+                "msg"=> ""
+            ],
+            "data"=>[
+                "access_token"=>$token,
+                "token_type"=> "Bearer"
+            ],
+            "count"=> 1
+        ], 200);
+    
+    
+    }
+
+    public function me(Request $request) {
+        return response()->json( [
+            "success"=> true,
+            "errors"=>[
+                "code"=>200,
+                "msg"=> ""
+            ],
+            "data"=>$request->user(),
+            "count"=> 1
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,8 @@
 <?php
 
-use App\Http\Controllers\PersonaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,10 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:sanctum');
 
-
-Route::post('/nuevousuario', [PersonaController::class, 'createUser'])->name('createUser');
+Route::post('/nuevousuario', [AuthController::class,"register"]);
+Route::post('/login', [AuthController::class,"login"]);
+Route::post('/usuario', [AuthController::class,"me"])->middleware('auth:sanctum');
 
